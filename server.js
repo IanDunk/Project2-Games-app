2dc1f61ffbffbfaa34fd58a27ff87047cec38d1c
Kathy Mai <kathymai@Kathys-MacBook-Air.local>
2018-01-22 14:30:17 -0800

    Socket connections

--- server.js
+++ server.js
@@ -6,10 +6,11 @@
 // =============================================================
 var express = require("express");
 var bodyParser = require("body-parser");
-
-// Sets up the Express App
-// =============================================================
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+// =============================================================
 var PORT = process.env.PORT || 8080;
 
 // Requiring our models for syncing
@@ -22,8 +23,22 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
+
+// Listener for incoming Socket connections 
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+    io.emit('new message', {msg: msg});
+  });
+});
+
 // Static directory
 app.use(express.static("public"));
+
+// Home page route
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
 
 // Routes
 // =============================================================
@@ -33,7 +48,10 @@ require("./routes/html-routes.js")(app);
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
 db.sequelize.sync({ force: true }).then(function() {
-  app.listen(PORT, function() {
+  http.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
 });
+
+
+    
